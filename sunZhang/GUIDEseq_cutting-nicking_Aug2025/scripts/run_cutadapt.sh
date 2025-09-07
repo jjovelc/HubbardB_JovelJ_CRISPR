e62ee9fc8d0e05eecb95b9c0d5d3bd4396e22f17
jjovelc <juan.jovel@ucalgary.ca>
2025-09-07 15:48:46 -0600

    sun directory deleted

--- sunZhang/GUIDEseq_cutting-nicking_Aug2025/scripts/run_cutadapt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-for FILE in *[1-2]/*_merged.fastq.gz
-do
-	cutadapt -j 8 --nextseq-trim=20 -e 0.10 -O 16 --discard-untrimmed \
-	-a 'ATGGTGAGCAAGGGCGAGG...GCATGGACGAGCTGTACAAGTAA' \
-	-o "${FILE/_merged.fastq.gz/_merged.extracted.fastq.gz}" \
-	"$FILE"
-done
